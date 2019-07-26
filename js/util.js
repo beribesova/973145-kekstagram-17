06474ce1b80c0da91c1a6ab199d84849d502c832
beribesova <beribesova@yandex.ru>
2019-07-26 12:03:40 +0300

    delete debounceTwo

--- js/util.js
+++ js/util.js
@@ -30,12 +30,6 @@
         window.clearTimeout(lastTimeout);
       }
       lastTimeout = window.setTimeout(debounceFunction.bind(null, data), debounceTime);
-    },
-    debounceTwo: function (debounceFunction, debounceTime) {
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(debounceFunction, debounceTime);
     }
   };
 })();
